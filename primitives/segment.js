1d6bebdfa63f70bd6a5ac8966e5c9ae87f89625c
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-06 00:42:46 +0530

    Update segment.js

--- primitives/segment.js
+++ primitives/segment.js
@@ -3,4 +3,12 @@ class Segment {
         this.p1 = p1;
         this.p2 = p2;
     }
+    draw(ctx, width = 2, color = "black") {
+        ctx.beginpath();
+        ctx.lineWidth = width;
+        ctx.strokeStyle = color;
+        ctx.moveTo(this.p1.x, this.p1.y);
+        ctx.lineTo(this.p2.x, this.p2.y);
+        ctx.stroke();
+    }
 }
