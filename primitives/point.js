73c703bfd45aa0eb4df8e8ef822effab2a50e481
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-06 00:36:25 +0530

    Update point.js

--- primitives/point.js
+++ primitives/point.js
@@ -1,7 +1,13 @@
 class Point {
     constructor(x, y) {
         this.x = x;
-        this.y = y;
-        
+        this.y = y;   
+    }
+    draw(ctx, size = 18, color= "black") {
+        const rad = size / 2;
+        ctx.beginPath();
+        ctx.fillstyle = color;
+        ctx.arc(this.x, this.y, rad, 0, Math.PI * 2)
+        ctx.fill();
     }
 }
