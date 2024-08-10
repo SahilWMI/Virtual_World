15c00b67004b1c14755cb14b28f93fe771a77e06
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-10 23:56:59 +0530

    Update graph.js

--- math/graph.js
+++ math/graph.js
@@ -3,10 +3,13 @@ class Graph{
         this.points = points;
         this.segments = segments;
     }
-}
-draw(ctx) {
-    for (const seg of this.segments) {
-        seg.draw(ctx);
-    }
+    draw(ctx) {
+        for (const seg of this.segments) {
+            seg.draw(ctx);
+        }
     
+        for (const point of this.points) {
+            point.draw(ctx);
+        }
+    }
 }
