c440652e5b6052313079ebfd8b32897c3b480cba
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-17 23:51:02 +0530

    Update graph.js

--- math/graph.js
+++ math/graph.js
@@ -20,6 +20,10 @@ class Graph{
         return false;
     }
 
+    removepoint(point) {
+        this.points.splice(this.points.indexOf(point), 1);
+    }
+
     addSegment(seg) {
         this.segments.push(seg);
     }
