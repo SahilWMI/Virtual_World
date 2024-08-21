8cf459db9536f0a8705e30b4204c27f11effc2a7
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-22 00:02:40 +0530

    Update graphEditor.js

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -2,8 +2,19 @@ class GraphEditor {
     constructor(canvas, graph) {
         this.canvas = canvas;
         this.graph = graph;
+
         this.ctx = this.canvas.getContext("2d");
+
+        this.#addEventListeners();
+
     }
+
+    #addEventListeners(){
+        this.canvas.addEventListener("mousedown", (evt) => {
+            const mouse = new PointerEvent(evt.offsetX, evt.offsetY);
+        });
+    }
+
     display() {
         this.graph.draw(this.ctx);
     }
